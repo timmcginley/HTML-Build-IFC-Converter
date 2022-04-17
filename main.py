73118dd1b6b9972f3391d1d3985fea99ee79961e
Tim McGinley <timwasnever@gmail.com>
2022-04-17 13:59:43 +0200

    send it to the internet

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import ifcopenshell
 
 model = ifcopenshell.open("model/Duplex_A_20110907_optimized.ifc")
-f = open("index.html", "w")
+f = open("output/index.html", "w")
 content=""
 # this just gets you the entity, defined here as wall
 # feel free to change this to your needs
+
 # ADD HTML
 content+="<html>\n"
 # ADD HEAD
 content+="\t<head>\n"
-content+="<link rel='stylesheet' href='css/html-build.css'></link>\n"
+content+="\t\t<link rel='stylesheet' href='css/html-build.css'></link>\n"
 content+="\t\tput some links in here...\n"
 content+="\t</head>\n"
 # ADD BODY
-content+="\t<body->\n"
+content+="\t<body>\n"
 # ADD PROJECT
 project = model.by_type('IfcProject')[0]
 content+="\t\t<project- name=\"{d}\">\n".format(d=project.LongName)
@@ -21,21 +22,21 @@ content+="\t\t<project- name=\"{d}\">\n".format(d=project.LongName)
 # ADD SITE
 content+="\t\t\t<site->\n"
 # ADD BUILDING
-content+="\t\t\t<building->\n"
+content+="\t\t\t\t<building->\n"
 # ADD CORE
-content+="\t\t\t<core->\n"
+content+="\t\t\t\t\t<core->\n"
 
 # ADD FLOOR(S)
 floors = model.by_type('IfcBuildingStorey')
 for floor in floors:
-    content+="\t\t\t<floor->floor</floor->\n"
+    content+="\t\t\t\t\t\t<floor->{}</floor->\n".format(floor.Name)
     
 # CLOSE IT ALL
-content+="\t\t\t</building->\n"
+content+="\t\t\t\t\t</core->\n"
+content+="\t\t\t\t</building->\n"
 content+="\t\t\t</site->\n"
 content+="\t\t</project->\n"
-content+="\t\t\t</core->\n"
-content+="\t</body->\n"
+content+="\t</body>\n"
 content+="</html>\n"
 
 # WRITE IT OUT
