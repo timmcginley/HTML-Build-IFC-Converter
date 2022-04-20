3ded18b4b18c59de4ad0af27920f1130eeab5d12
Tim McGinley <timwasnever@gmail.com>
2022-04-20 11:38:00 +0200

    more style on the floors

--- main.py
+++ main.py
@@ -1,36 +1,65 @@
+# First try to extract basic info from an IFC file and create an HTML file from this.
+# This is focused on the use case of tall buildings
+# The idea is that it can read the IFC and give a simple 2d representation
+# This can then be used to provide feedback to the user.
+# It can also be displayed on the users homepage and in their teams group.
+
+# obviously we need to ...
 import ifcopenshell
 
+# get the IFC file
 model = ifcopenshell.open("model/Duplex_A_20110907_optimized.ifc")
+
+# create an HTML file to write to
 f = open("output/index.html", "w")
 content=""
+
 # this just gets you the entity, defined here as wall
 # feel free to change this to your needs
+site_elevation = 0
 
 # ADD HTML
 content+="<html>\n"
+
 # ADD HEAD
 content+="\t<head>\n"
 content+="\t\t<link rel='stylesheet' href='css/html-build.css'></link>\n"
 content+="\t\tput some links in here...\n"
 content+="\t</head>\n"
+
 # ADD BODY
 content+="\t<body>\n"
+
 # ADD PROJECT
 project = model.by_type('IfcProject')[0]
 content+="\t\t<project- name=\"{d}\">\n".format(d=project.LongName)
-# it looks like it would make sense to use the dom here and append stuff to it...
+# it looks like it would make sense to use the DOM here and append stuff to it...
+
 # ADD SITE
-content+="\t\t\t<site->\n"
+site = model.by_type('IfcSite')[0]
+site_elevation = site.RefElevation
+content+="\t\t\t<site- lat=\"{}\" long=\"{}\" elev=\"{}\">\n".format(site.RefLatitude,site.RefLongitude,site_elevation )
+
 # ADD BUILDING
 content+="\t\t\t\t<building->\n"
-# ADD CORE
+
+# ADD CORE - I know its not normal,  but I think it might be useful...
 content+="\t\t\t\t\t<core->\n"
 
 # ADD FLOOR(S)
 floors = model.by_type('IfcBuildingStorey')
 for floor in floors:
-    content+="\t\t\t\t\t\t<floor->{}</floor->\n".format(floor.Name)
-    
+    # check if floor is lower than elevation...
+    # TODO: we need to sort these, IFC doesn't do it automatically...
+
+    if (site_elevation == floor.Elevation):
+        content+="\t\t\t\t\t\t<floor- class=\"floor_ground\" height=\"{}\" >GROUND {}</floor->\n".format(floor.Elevation, floor.Name)
+    elif (site_elevation < floor.Elevation):
+        content+="\t\t\t\t\t\t<floor- class=\"floor_upper\" height=\"{}\" >upper {}</floor->\n".format(floor.Elevation, floor.Name)
+    else:
+        content+="\t\t\t\t\t\t<floor- class=\"floor_lower\" height=\"{}\" >lower {}</floor->\n".format(floor.Elevation, floor.Name)
+
+
 # CLOSE IT ALL
 content+="\t\t\t\t\t</core->\n"
 content+="\t\t\t\t</building->\n"
@@ -45,4 +74,4 @@ f.close()
 
 # TELL EVERYONE ABOUT IT
 print("html build complete")
-# ###################### end of example ###########################
+
